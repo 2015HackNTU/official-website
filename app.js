5fb9756959cef94403b7a1b4349b92fe9ece96d2
lulalachen <lulalachen@gmail.com>
2015-01-26 13:59:41 +0800

    typo

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var news = require('./routes/news');
 var app = express();
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views/client'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
