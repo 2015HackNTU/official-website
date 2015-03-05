51db3d6a496815fc4efbb3f3c6bd2d617d41ec24
bun919tw <bun919tw@gmail.com>
2015-03-06 01:52:12 +0800

    finish course page

--- views/client/js/course.js
+++ views/client/js/course.js
@@ -1,1 +1,49 @@
-aa
+$(function() {
+	$('#web').on("click", function(){
+		$('#web_teacher').removeClass('hidden');
+		$('#iot_teacher').addClass('hidden');
+		$('#app_teacher').addClass('hidden');
+		$('#uiux_teacher').addClass('hidden');
+
+		$('#web_slide').removeClass('hidden');
+		$('#iot_slide').addClass('hidden');
+		$('#app_slide').addClass('hidden');
+		$('#uiux_slide').addClass('hidden');
+	});
+
+	$('#iot').on("click", function(){
+		$('#web_teacher').addClass('hidden');
+		$('#iot_teacher').removeClass('hidden');
+		$('#app_teacher').addClass('hidden');
+		$('#uiux_teacher').addClass('hidden');
+
+		$('#web_slide').addClass('hidden');
+		$('#iot_slide').removeClass('hidden');
+		$('#app_slide').addClass('hidden');
+		$('#uiux_slide').addClass('hidden');
+	});
+
+	$('#app').on("click", function(){
+		$('#web_teacher').addClass('hidden');
+		$('#iot_teacher').addClass('hidden');
+		$('#app_teacher').removeClass('hidden');
+		$('#uiux_teacher').addClass('hidden');
+
+		$('#web_slide').addClass('hidden');
+		$('#iot_slide').addClass('hidden');
+		$('#app_slide').removeClass('hidden');
+		$('#uiux_slide').addClass('hidden');
+	});
+
+	$('#uiux').on("click", function(){
+		$('#web_teacher').addClass('hidden');
+		$('#iot_teacher').addClass('hidden');
+		$('#app_teacher').addClass('hidden');
+		$('#uiux_teacher').removeClass('hidden');
+
+		$('#web_slide').addClass('hidden');
+		$('#iot_slide').addClass('hidden');
+		$('#app_slide').addClass('hidden');
+		$('#uiux_slide').removeClass('hidden');
+	});
+});
