f29c8c6348c73838a1b51fcaba596ac6f50074d7
pilagod <r03725029@ntu.edu.tw>
2015-03-25 12:57:38 +0800

    home beta

--- routes/pages/home/route_home.js
+++ routes/pages/home/route_home.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.send('home/home');
+    res.render('home/home');
 });
 
 module.exports = router;
