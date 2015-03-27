a1507761dacafd89f27a657d3aa9101b94c87062
Wayne Huang <wayne123tw@gmail.com>
2015-03-28 01:56:44 +0800

    add webfont and add community function

--- public/js/community/script.js
+++ public/js/community/script.js
@@ -11,4 +11,16 @@ $(document).ready(function () {
 			$(this).siblings('.frame-outer').addClass('animated flipInY').show();
 		});
 	});
+	$('.timeline-tag').click(function () {
+		tag = $(this).attr('tag');
+		$('.activity#' + tag).animate({
+			bottom: '-20px'
+		}, 1000);
+	});
+	$('.cross').click(function () {
+		tag = $(this).attr('tag');
+		$('.activity#' + tag).animate({
+			bottom: '-700px'
+		}, 1000);
+	});
 });
