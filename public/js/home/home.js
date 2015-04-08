4cd81dd66f40f413496ec461df7a21c47aad319c
pilagod <r03725029@ntu.edu.tw>
2015-04-08 21:15:20 +0800

    home responsive ver1

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -2,12 +2,15 @@
  * Created by pilagod on 4/4/15.
  */
 $(document).ready(function(){
+
+    $('#navbar>nav').addClass('navbar-hide');
+
     $('#top-anchor').waypoint({
         handler: function(direction){
             if(direction === "down"){
 //                $('#top-nav').removeClass('top-nav-before');
-                $('#fb').css('font-size', '1.5em');
-                $('#youtube').css('font-size', '1.5em');
+                $('#fb-icon').css('font-size', '1.5em');
+                $('#youtube-icon').css('font-size', '1.5em');
                 $('#top-nav-logo').addClass('top-nav-logo-after');
                 $('#top-nav-hackntu').addClass('top-nav-hackntu-after');
                 $('#top-nav').addClass('top-nav-after');
@@ -15,8 +18,8 @@ $(document).ready(function(){
             }
             else{
                 $('#top-nav').removeClass('top-nav-after');
-                $('#fb').css('font-size', '2em');
-                $('#youtube').css('font-size', '2em');
+                $('#fb-icon').css('font-size', '2em');
+                $('#youtube-icon').css('font-size', '2em');
                 $('#top-nav-logo').removeClass('top-nav-logo-after');
                 $('#top-nav-hackntu').removeClass('top-nav-hackntu-after');
 //                $('#top-nav').addClass('top-nav-before');
@@ -28,16 +31,19 @@ $(document).ready(function(){
         handler: function(direction){
             if(direction === "down"){
                 $('#top-nav').addClass('navbar-hide');
-                $('#navbar').removeClass('navbar-hide');
+                //$('#navbar').removeClass('navbar-hide');
+                $('#navbar>nav').removeClass('navbar-hide');
             }
             else{
                 $('#top-nav').removeClass('navbar-hide');
-                $('#navbar').addClass('navbar-hide');
+                //if(!$('#youtube-div').hasClass('show'))
+                //    $('#navbar').addClass('navbar-hide');
+                $('#navbar>nav').addClass('navbar-hide');
             }
         }
     });
 
-    $.waypoints('refresh');
+    //$.waypoints('refresh');
 
 //    $('#nav-anchor').waypoint({
 //        handler: function(direction){
