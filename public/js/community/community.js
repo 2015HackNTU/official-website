a2873485ec4f28263c31b07980e4230db2005d24
PuChin <puching110@gmail.com>
2015-05-02 16:28:34 +0800

    Community:
    1.Adjust month length/tag position
    2.Document at the top can compatible Safari/Chrome
    3.Complete the content for each pop-up window

--- public/js/community/community.js
+++ public/js/community/community.js
@@ -8,23 +8,46 @@ app.controller('communityCtrl', ['$scope', function ($scope) {
 		//Event here
 		{date:'3/7', name:'網路前端設計', title: '網路前端工作坊',
 		 content: "<p>誰說沒有技術背景的人不能成為hacker？</p>\
-<p>我們相信只要有心，每個人都能成為自己動手解決問題的hacker。以一天密集的課程帶領不曾有過coding經驗，但擁有熱忱的學員踏出自己動手做的第一步。由HackNTU創辦人張凱迪擔任講師，借用他豐富的教學經驗與文組轉為工程師的特殊經歷，以過來人角度點燃初學者心中的hacker spirit。</p>\
-<p>除此之外，透過工作坊，學員結識其他同樣朝向黑客之路的人，於活動結束後，一同持續精進實力。</p>"},
-		{date:'3/28', title: '網頁 x 社群 x 半途出家 Meetup'},
-		{date:'5/3', name:'軟硬整合-相機', title: 'Hack into the City - Raspberry Pi 相機 IoT Workshop',
-		 content: "<p>在這個資訊爆炸，科技發達的時代，人們的生活步調越來越快，IoT(物聯網)是未來資訊技術在運算與溝通上的演進趨勢，而這樣的演進過程中將會需要各式各樣領域的技術及科技創新來帶動，小從奈米科技、大至城市無線網路的佈建，其影響範圍相當廣泛。</p>\
-  <p>今年二月，Raspberry pi基金會正推出新一代的Raspberry pi 2，搭載了時脈為900MHz的ARM四核心Cortex-A7處理器，使處理速度是前代的六倍，更可以支援microsoft新推出的Window 10作業系統，應用於物聯網開發之中。</p>\
-  <p>在本次工作坊之中，我們將會利用Raspberry pi Model B，這塊麻雀雖小，卻能發揮許多功用的板子，在上面寫些簡單的程式，搭配手機去控制相機，實現我們的需求，同時也能對於IoT這項概念能有所體驗。</p>\
-<p>連結：<a href='http://hackntuworkshop2.parseapp.com/'>http://hackntuworkshop2.parseapp.com/</a></p>"}
+					<p>我們相信只要有心，每個人都能成為自己動手解決問題<br>\
+					的hacker。以一天密集的課程帶領不曾有過coding經驗，<br>\
+					但擁有熱忱的學員踏出自己動手做的第一步。由HackNTU創辦人<br>\
+					張凱迪擔任講師，借用他豐富的教學經驗與文組轉為工程師<br>\
+					的特殊經歷，以過來人角度點燃初學者心中的hacker spirit。</p>\
+					<p>除此之外，透過工作坊，學員結識其他同樣朝向黑客之路的人，<br>\
+					於活動結束後，一同持續精進實力。</p>"},
+
+		{date:'3/28', name:'半途出家' ,title: '網頁 x 社群 x 半途出家 Meetup', 
+		content: "<p>一開始由 Ruby 的大大 — 高見龍，龍哥為<br>\
+					我們講述他高低起伏、充滿驚奇的程式學習歷程<br>\
+					；隨後也為我們帶來他對 Ruby 情有獨鍾的原因，並小露一手 Rails <br>\
+					快速開發的絕技，引起了大家一陣驚呼！</p>\
+					<p>中場休息後，由 HackNTU的好朋友、曾擔任第五屆 <br>\
+					Hackathon Taiwan總召的 Roger <br>\
+					為我們介紹他自身半路出家的故事。<br>\
+					，Roger 也對他所常用的 Node.js 進行了簡單的導覽，<br>\
+					並和大家分享過去擔任總召的辛酸血淚。</p>"},
+
+		{date:'5/3', name:'樹莓派相機工作坊', title: 'Raspberry Pi Camera Workshop',
+		 content: "<p>IoT(物聯網)是未來資訊技術在運算與溝通上的演進<br>\
+		 			趨勢，而這樣的演進過程中將會需要各式各樣領域<br>\
+		 			的技術及科技創新來帶動，小從奈米科技、大至城市<br>\
+		 			無線網路的佈建，其影響範圍相當廣泛。</p>\
+  					<p>在本次工作坊之中，我們將會利用<br>\
+ 					Raspberry pi Model B，這塊麻雀雖小，卻能發揮許多功用的<br>\
+ 					板子，在上面寫些簡單的程式，搭配手機去控制相機，<br>\
+ 					實現我們的需求，同時也能對於IoT這項概念能有所體驗。</p>"},
+ 					
+		{date:'5/28', name:'Data Science Meetup', title: 'Data Science / Open Data Meetup', 
+		content:'<h1>Coming soon</h1>'},
+
 	];
 
 	$scope.tags = [
 		//Timeline tags here
-		{date:'3/7,', picname:'activity-1', left: '100', background:'#BF1920'},
-		{date:'3/28', picname:'activity-2', left: '200', background:'#BF1920'},
-		{date:'5/3', picname:'activity-3', left: '300', background:'#BF1920'},
-		{date:'5/28', picname:'activity-4', left: '400', background:'#BF1920'},
-		{date:'7/4', picname:'activity-5', left: '500', background:'#BF1920'}
+		{date:'3/7,', picname:'activity-1', left: '160', background:'#BF1920'},//workshop
+		{date:'3/28', picname:'activity-2', left: '230', background:'#ED8216'},//meetup
+		{date:'5/3', picname:'activity-3', left: '400', background:'#BF1920'},//workshop
+		{date:'5/28', picname:'activity-4', left: '480', background:'#ED8216'}//meetup
 	];
 
 	$scope.clickTag = function(tag){
